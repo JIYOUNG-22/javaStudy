310838cf4e796fe82576693a5820efc052c88d23
JIYOUNG-22 <dyong0922@naver.com>
2024-08-03 15:18:16 +0900

    programmers.Level0

---
+++ src/main/java/com/dyong/programmers/level0/두수의합.java
@@ -0,0 +1,15 @@
+package com.dyong.programmers.level0;
+
+public class 두수의합 {
+    public static void main(String[] args) {
+        두수의합 addnum = new 두수의합();
+        System.out.println("두 수의 합 : "+addnum.solution(2,3));
+    }
+
+    public int solution(int num1, int num2) {
+
+        return num1 + num2;
+    }
+}
+
+

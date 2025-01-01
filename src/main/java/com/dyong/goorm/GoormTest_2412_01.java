041825e573e361eea1210e9b8e28bf38f881bc29
JIYOUNG-22 <dyong0922@naver.com>
2025-01-02 02:14:08 +0900

    250101 : 12월 구름 코딩테스트 (중급) -> 틀린 문제 수정

--- src/main/java/com/dyong/goorm/GoormTest_2412_01.java
+++ src/main/java/com/dyong/goorm/GoormTest_2412_01.java
@@ -2,10 +2,9 @@ package com.dyong.goorm;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Stack;
 
-/* 플레이데이터 2024년 12월 코딩테스트(중급) [점수: 0/30] */
+/* 플레이데이터 2024년 12월 코딩테스트(중급) [점수: 0/30] -> 수정 완료 */
 public class GoormTest_2412_01 {
     public static void main(String[] args) throws Exception {
 
@@ -13,22 +12,30 @@ public class GoormTest_2412_01 {
         int n = Integer.parseInt(br.readLine());
         char[] inputArr = br.readLine().toCharArray();
 
-        Queue<Integer> open = new LinkedList<>();
-        Queue<Integer> close = new LinkedList<>();
-        int result = 0;
+        Stack<String> input = new Stack<>();
+        Stack<String> temp = new Stack<>();
+        StringBuilder code = new StringBuilder();
 
         for(int i = 0; i < n; i++) {
 
-            if(inputArr[i] == '(') {
-                open.add(i);
-            } else if(inputArr[i] == ')') {
-
-                result += open.poll();
+            if(inputArr[i]=='(') {
+                input.push("(");
+            } else if(inputArr[i]==')') {
+                while(!input.peek().equals("(")){
+                    temp.push(input.pop());
+                }
+                input.pop();
+                while(!temp.isEmpty()) {
+                    code.append(temp.peek());
+                    input.push(temp.pop());
+                }
             } else {
-                result++;
+                input.push(String.valueOf(inputArr[i]));
+                code.append(inputArr[i]);
             }
 
-
         }
+        //System.out.println(code);
+        System.out.println(code.length());
     }
 }
